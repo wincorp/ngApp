78d496565e6c9151d12a96064746a3d9284bf9c9
wincorp <tom@wincorp.net>
2018-06-17 10:01:17 -0700

    added a-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AComponentComponent } from './a-component/a-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AComponentComponent
   ],
   imports: [
     BrowserModule,
