5c32c4d56b29d9706dfbff2057aba6bfe23675ef
wincorp <tom@wincorp.net>
2018-06-19 07:50:09 -0700

    component b

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AComponentComponent } from './a-component/a-component.component';
+import { BComponent } from './b/b.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AComponentComponent
+    AComponentComponent,
+    BComponent
   ],
   imports: [
     BrowserModule,
